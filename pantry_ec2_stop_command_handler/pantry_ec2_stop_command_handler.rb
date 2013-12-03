89eb7531b0d48dee14f31ce31fb454f9a6575761
Kieran Manning <kieran.manning@wonga.com>
2013-12-03 17:13:32 +0000

    logging

--- pantry_ec2_stop_command_handler/pantry_ec2_stop_command_handler.rb
+++ pantry_ec2_stop_command_handler/pantry_ec2_stop_command_handler.rb
@@ -7,9 +7,11 @@ module Wonga
       end
 
       def handle_message(message)
+        @logger.info("Received stop message for instance: #{message["name"]} - request_id: #{message["id"]}")
         ec2 = AWS::EC2.new
         instance = ec2.instances[message['instance_id']]
         instance.stop 
+        @logger.info("Stopped instance: #{message["name"]} - request_id: #{message["id"]} - publishing")
         @publisher.publish message
       end
     end
