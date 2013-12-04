9438fdedea626eb3c97082b79ab74e972e5296d2
Kieran Manning <kieran.manning@wonga.com>
2013-12-04 11:12:49 +0000

    updated

--- pantry_ec2_stop_command_handler/pantry_ec2_stop_command_handler.rb
+++ pantry_ec2_stop_command_handler/pantry_ec2_stop_command_handler.rb
@@ -10,9 +10,19 @@ module Wonga
         @logger.info("Received stop message for instance: #{message["name"]} - request_id: #{message["id"]}")
         ec2 = AWS::EC2.new
         instance = ec2.instances[message['instance_id']]
-        instance.stop 
-        @logger.info("Stopped instance: #{message["name"]} - request_id: #{message["id"]} - publishing")
-        @publisher.publish message
+        case instance.status
+        when :stopped 
+          @logger.info("Stopped instance: #{message["name"]} - request_id: #{message["id"]} - publishing")
+          @publisher.publish message          
+          return
+        when :terminated
+          @logger.error("Attempted to stop terminated instance: #{message["name"]} - id #{message["id"]}")
+          return
+        when :running
+          @logger.info("Stopping instance: #{message["name"]} - request_id: #{message["id"]} - publishing")
+          instance.stop
+        end
+        raise "Instance #{message['instance_id']} still pending"
       end
     end
   end
--- spec/pantry_ec2_stop_command_handler/pantry_ec2_stop_command_handler_spec.rb
+++ spec/pantry_ec2_stop_command_handler/pantry_ec2_stop_command_handler_spec.rb
@@ -2,13 +2,13 @@ require 'spec_helper'
 require_relative '../../pantry_ec2_stop_command_handler/pantry_ec2_stop_command_handler'
 
 describe Wonga::Daemon::PantryEc2StopCommandHandler do
-  let(:publisher) { instance_double('Wonga::Publisher').as_null_object }
-  let(:logger) { instance_double('Logger').as_null_object }
-  let(:instance) { instance_double('AWS::EC2::Instance').as_null_object }
-  let(:ec2) { instance_double('AWS::EC2') }
+  let(:publisher) { instance_double('Wonga::Publisher').as_null_object   }
+  let(:logger)    { instance_double('Logger').as_null_object             }
+  let(:instance)  { instance_double('AWS::EC2::Instance').as_null_object }
+  let(:ec2)       { instance_double('AWS::EC2')                          }
 
   subject do 
-    described_class.new(publisher, logger).as_null_object
+    described_class.new(publisher, logger)
   end
   
   it_behaves_like 'handler'
@@ -17,16 +17,40 @@ describe Wonga::Daemon::PantryEc2StopCommandHandler do
     before(:each) do
       AWS::EC2.stub(:new).and_return(ec2)
       ec2.stub(:instances).and_return(instance)
+      publisher.stub(:publish)
     end
 
-    it "calls instance.stop" do
-      instance.should_receive(:stop).and_return(nil) # even on success returns nil
-      subject.handle_message({"instance_id"=>"i-6c3db923"})
+    context "Machine stopped" do 
+      it "should publish" do 
+        instance.stub(:status).and_return(:stopped)
+        publisher.should_receive(:publish)
+        subject.handle_message({"instance_id"=>"i-3245243"})
+      end
     end
-    
-    it "publishes a stopped event" do
-      publisher.stub(:publish).with({"instance_id" => "i-6c3db923"})
-      subject.handle_message({"instance_id"=>"i-6c3db923"})
+
+    context "Machine terminated" do 
+      it "should log an error" do 
+        instance.stub(:status).and_return(:terminated)
+        logger.should_receive(:error)
+        subject.handle_message({"instance_id"=>"i-3245243"})
+      end
+    end
+
+    context "Machine running" do 
+      it "Attempts to stop the instance" do 
+        instance.stub(:status).and_return(:running)
+        expect{
+          subject.handle_messsage({"instance_id"=>"i-3245243"})
+        }.to raise_error
+      end
+    end
+
+    context "Otherwise(pending)" do 
+      it "raises a benign error" do 
+        expect{
+          subject.handle_messsage({"instance_id"=>"i-3245243"})
+        }.to raise_error
+      end
     end
   end
 end
